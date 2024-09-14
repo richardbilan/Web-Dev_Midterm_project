7edd5af8cf0adb003c6d361abe9df6b6f671783a
richardbilan <157667872+richardbilan@users.noreply.github.com>
2024-09-14 12:48:47 +0800

    updatee

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,13 +3,14 @@
   <head>
     <meta charset="UTF-8" />
     <title>Responsive Login Form HTML CSS | CodingNepal</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="{{ asset('styles/style.css') }}" />
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
   <body>
-    <div class="container">
-      <form ><a href="{{ route('about') }}">About</a></
+  <div class="container">
+      <form action="{{ route('login') }}" method="POST">
+        <a href="{{ route('about') }}">About</a>
         <div class="title">Login</div>
         <div class="input-box underline">
           <input type="text" placeholder="Enter Your Email" required />
@@ -29,6 +30,9 @@
       </div>
       <div class="facebook">
         <a href="#"><i class="fab fa-facebook-f"></i>Sign in With Facebook</a>
+      </div>
+      <div class="signup-link">
+        Don't have an account? <a href="{{ route('sign up') }}">Sign up</a>
       </div>
     </div>
   </body>
