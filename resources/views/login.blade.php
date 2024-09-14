821531651764067594941f2326e099275c80c134
theaeou <157953371+theaeou@users.noreply.github.com>
2024-09-14 15:35:56 +0800

    log in & sign up changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8" />
-    <title>Responsive Login Form HTML CSS | CodingNepal</title>
+    <title>Log In</title>
     <link rel="stylesheet" href="{{ asset('styles/style.css') }}" />
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,7 +10,6 @@
   <body>
   <div class="container">
       <form action="{{ route('login') }}" method="POST">
-        <a href="{{ route('about') }}">About</a>
         <div class="title">Login</div>
         <div class="input-box underline">
           <input type="text" placeholder="Enter Your Email" required />
@@ -26,14 +25,14 @@
       </form>
       <div class="option">or Connect With Social Media</div>
       <div class="twitter">
-        <a href="#"><i class="fab fa-twitter"></i>Sign in With Twitter</a>
+        <a href="#"><i class="fab fa-twitter"></i>Sign in With Google</a>
       </div>
       <div class="facebook">
         <a href="#"><i class="fab fa-facebook-f"></i>Sign in With Facebook</a>
       </div>
       <div class="signup-link">
-        Don't have an account? <a href="">Sign up</a>
-      </div>
+    Don't have an account? <a href="{{ url('/signup') }}">Sign up</a>
+</div>
     </div>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,49 +6,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome'); // Name the route
+})->name('welcome'); 
 
 Route::get('/contactus', function () {
     return view('contactus');
-})->name('contact'); // Name the route
+})->name('contact'); 
 
 Route::get('/about', function () {
     return view('about');
-})->name('about'); // Name the route
+})->name('about'); 
 
 Route::get('/user/{name?}', function ($name = null) {
     $userName = preg_replace('/[^a-zA-Z]/', '', $name);
-
     $userName = $userName ?: 'Guest';
-
     $userName = ucfirst(strtolower($userName));
-
     session()->flash('user_name', $userName);
 
-    return redirect()->route('welcome'); // Redirect to named route
+    return redirect()->route('welcome');
 });
-
 // Display the login form
 Route::get('/login', function () {
-    return view('login'); // Make sure 'login' corresponds to 'resources/views/login.blade.php'
+    return view('login');
 })->name('login');
 
-// Handle the login form submission
-Route::post('/login', function (Request $request) {
-    // Validate the form data
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
-
-    // Attempt to log the user in
-    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-        // Redirect to the intended page or a dashboard
-        return redirect()->intended('/dashboard');
-    }
-
-    // If login fails, redirect back with an error message
-    return back()->withErrors([
-        'email' => 'The provided credentials do not match our records.',
-    ]);
-});
+// Display the signup form
+Route::get('/signup', function () {
+    return view('signup'); 
+})->name('signup');
